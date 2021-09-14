47e6cf2c6fa67c5ed0ed43b5a7a54795d62822de
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-14 17:04:09 +0200

    refactored the fizzbuz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    'fizzbuzz'
-  elsif number % 3 == 0
-    'fizz'
-  elsif number % 5 == 0
-    'buzz'
-  end
+  output = ""
+  output += 'fizz' if number % 3 == 0
+  output += 'buzz' if number % 5 == 0 
+  return output 
 end
+
